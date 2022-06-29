521b58946b4c63930cf47c96068fdd38f0cc60a0
z00mni <chjm219@kookmin.ac.kr>
2022-06-29 14:25:57 +0000

    level4 - 자동완성

---
+++ level4/1.js
@@ -0,0 +1,32 @@
+function makeTrie(words) {
+    const root = {}
+    
+    for (const word of words) {
+        let current = root;
+        for (letter of word) {
+            if (!current[letter]) current[letter] = [0, {}]
+            else {
+                current[letter][0] = 1 + (current[letter][0] || 0)
+                current = current[letter][1]
+            }
+        }
+    }
+    return root;
+}
+
+function solution(words) {
+    var answer = 0;
+    const trie = makeTrie(words);
+    
+    for (const word of words) {
+        let count = 0;
+        let current = trie;
+        for (const [index, letter] of [...word].entries()) {
+            count++;
+            if (current[letter][0] <= 1) break;
+            current = current[letter][1];
+        }
+        answer += count;
+    }
+    return answer;
+}
